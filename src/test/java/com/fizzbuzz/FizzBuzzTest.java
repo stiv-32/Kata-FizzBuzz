792bfa9af4327b52aa85f6962dbca5628bc255f2
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2022-02-01 14:15:49 +0100

    init repo

---
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    
+    @Test
+    public void test_fizzbuzz_example() {
+        assertEquals(2, 2);
+    }
+
+}
